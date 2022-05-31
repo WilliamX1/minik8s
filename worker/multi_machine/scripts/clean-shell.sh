e1f2d62afdd0ff2da058f68cd3458bdd677b77bd
xu-huidong <HuiD_Xu@163.com>
2022-05-31 22:03:54 +0800

    Prepare for Multi-Machine

--- worker/multi_machine/scripts/clean-shell.sh
+++ worker/multi_machine/scripts/clean-shell.sh
@@ -2,6 +2,7 @@
 
 # clean up residual processes
 
+ps aux | grep python3 | awk '{print $2}' | xargs kill -9
 ps aux | grep etcd | awk '{print $2}' | xargs kill -9
 ps aux | grep flannel | awk '{print $2}' | xargs kill -9
 ps aux | grep docker | awk '{print $2}' | xargs kill -9
