0355487508fd16ae33dc5f46b217c3e109e22f37
xu-huidong <HuiD_Xu@163.com>
2022-05-31 21:21:41 +0800

    Add Shell Command into kubelet_flask.py

---
+++ worker/multi_machine/scripts/clean-shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# clean up residual processes
+
+ps aux | grep etcd | awk '{print $2}' | xargs kill -9
+ps aux | grep flannel | awk '{print $2}' | xargs kill -9
+ps aux | grep docker | awk '{print $2}' | xargs kill -9
