966a5926c35fc15121ff6402bc65e1f8ccac640e
xu-huidong <HuiD_Xu@163.com>
2022-06-01 13:07:32 +0800

    update

--- worker/multi_machine/scripts/flannel-shell.sh
+++ worker/multi_machine/scripts/flannel-shell.sh
@@ -8,4 +8,4 @@ ETCD_ENDPOINTS=$2
 ${BASE_DIR}/../etcd/etcdctl put /coreos.com/network/config < ${BASE_DIR}/../configs/flannel-network-config.json
 
 # start flanneld
-sudo ${BASE_DIR}/../flanneld
+sudo ${BASE_DIR}/../flanneld -iface=ens3
