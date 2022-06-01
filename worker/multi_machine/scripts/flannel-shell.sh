52c792f29b451336c89a869cf54279e2e1c4105e
xu-huidong <HuiD_Xu@163.com>
2022-06-01 11:33:17 +0800

    update

--- worker/multi_machine/scripts/flannel-shell.sh
+++ worker/multi_machine/scripts/flannel-shell.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 IP_ADDRESS=$1
 
 # config flannel
-${BASE_DIR}/../etcd/etcdctl set /coreos.com/network/config < ${BASE_DIR}/../configs/flannel-network-config.json
+ETCDCTL_API=3 ${BASE_DIR}/../etcd/etcdctl put /coreos.com/network/config < ${BASE_DIR}/../configs/flannel-network-config.json
 
 # start flanneld
 sudo ${BASE_DIR}/../flanneld -iface=${IP_ADDRESS}
